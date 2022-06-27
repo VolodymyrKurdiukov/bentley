4a5d8cb4ffb2f7d348b47667763fc55cc4aa30fe
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-06-27 15:18:28 +0300

    anim+fullscrenn2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,5 @@
+
+
 //ibg
 
 function ibg() {
@@ -10,15 +12,77 @@ function ibg() {
 }
 ibg();
 
+
 //=====================================================================================================
 
 //Burger
 
 $(document).ready(function () {
 	$('.header__burger').click(function (event) {
-		$('.header__burger,.header__menu').toggleClass('active');
+		$('.header__burger,.header__menu,.header__label,.fullscreen2__content').toggleClass('active');
 		$('body').toggleClass('lock');
+	});
+
+	//=====================================================================================================
+
+	//animated scroll 
+
+	// $("a[href^='#']").click(function () {
+	// 	const _href = $(this).attr("href");
+	// 	$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+	// 	return false;
+	// });
+
+	//=====================================================================================================
+
+	//goto block
+
+	$('.goto').click(function () {
+		var el = $(this).attr('href').replace('#', '');
+		var offset = 0;
+		$('body,html').animate({ scrollTop: $('.' + el).offset().top + offset }, 500, function () { });
+	});
+
+		//=====================================================================================================
+
+//active point scroll
+	$(document).on("scroll", onScroll);
+
+	//smoothscroll
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+		$(document).off("scroll");
+
+		$('a').each(function () {
+			$(this).removeClass('active');
+		})
+		$(this).addClass('active');
+
+		var target = this.hash,
+			menu = target;
+		$target = $(target);
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top + 2
+		}, 800, 'swing', function () {
+			window.location.hash = target;
+			$(document).on("scroll", onScroll);
+		});
 	});
 });
 
+
+function onScroll(event) {
+	var scrollPos = $(document).scrollTop();
+	$('.decor__circle').each(function () {
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.decor__circle').removeClass("active");
+			currLink.addClass("active");
+		}
+		else {
+			currLink.removeClass("active");
+		}
+	});
+}
 //=====================================================================================================
